b6e5136729583b5a4731941a215080d6df8e1112
ryhara <aaxouei52@gmail.com>
2023-05-20 09:41:34 +0900

    [Add] strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryhara <ryhara@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 22:50:51 by ryhara            #+#    #+#             */
+/*   Updated: 2023/05/19 22:50:51 by ryhara           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*little == '\0')
+		return ((char *)big);
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] && little[j] && (i + j) < len)
+		{
+			if (big[i + j] == little[j])
+				j++;
+			else
+				break ;
+		}
+		if (j == ft_strlen(little))
+			return ((char *)(big + i));
+		i++;
+	}
+	return (NULL);
+}
+
+#include<stdio.h>
+#include<string.h>
+int	main(void)
+{
+	printf("%s\n",ft_strnstr("aiueo","ue",4));
+}
