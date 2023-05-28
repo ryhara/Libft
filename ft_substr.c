9342c51fa7eb3b728e541afc0d788471bde06a09
ryhara <aaxouei52@gmail.com>
2023-05-28 14:48:27 +0900

    [Add] substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,35 +6,42 @@
 /*   By: ryhara <ryhara@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 14:49:59 by ryhara            #+#    #+#             */
-/*   Updated: 2023/05/21 16:10:45 by ryhara           ###   ########.fr       */
+/*   Updated: 2023/05/28 14:47:56 by ryhara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char *subs;
-	size_t	subs_len;
+	char	*subs;
 	size_t	i;
+	size_t	s_len;
 
 	i = 0;
-	subs_len = len + 1;
-	subs = (char *)malloc(sizeof(char) * subs_len);
+	if (s == NULL || len == 0)
+		return (NULL);
+	if (ft_strlen(s) < (size_t)start)
+		return (NULL);
+	subs = (char *)malloc(sizeof(char) * (len + 1));
 	if (!subs)
 		return (NULL);
-	while (i < subs_len)
+	while (s[start + i] != '\0' && i < len)
 	{
 		subs[i] = s[start + i];
 		i++;
 	}
-	return(subs);
+	subs[i] = '\0';
+	return (subs);
 }
 
 // #include<stdio.h>
 // int	main(void)
 // {
-// 	char *s1;
-// 	s1 = ft_substr("abcdefghijk",5 ,3);
-// 	printf("%s\n",s1);
+// 	char	*s1;
+// 	s1 = ft_substr("libft-test-tokyo", 0 ,0);
+// 	if (s1 == NULL)
+// 		printf("null\n");
+// 	else
+// 		printf("%s\n",s1);
 // }
