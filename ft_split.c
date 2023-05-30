e39933ec89451311c8ddd9fc02d356c24a40ac88
ryhara <aaxouei52@gmail.com>
2023-05-30 22:25:42 +0900

    [Fix] split

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: ryhara <ryhara@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 16:42:10 by ryhara            #+#    #+#             */
-/*   Updated: 2023/05/22 18:25:36 by ryhara           ###   ########.fr       */
+/*   Updated: 2023/05/30 22:24:32 by ryhara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	word_count(char *str, char c)
+static size_t	word_count(char *str, char c)
 {
 	size_t	count;
 
@@ -28,88 +28,81 @@ size_t	word_count(char *str, char c)
 	return (count);
 }
 
-size_t	check_start(char *str, char c, size_t i)
+static void	free_all(char **array, size_t len)
 {
-	while (str[i] == c)
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		free(array[i]);
 		i++;
-	return (i);
+	}
+	free(array);
 }
 
-size_t	check_end(char *str, char c, size_t i)
-{
-	while (str[i] && str[i] != c)
-		i++;
-	return (i);
-}
-
-char	**do_split(char **arr, char *str, char c, size_t arr_len)
+static char	**do_split(char **array, char *str, char c, size_t arr_len)
 {
 	size_t	i;
 	size_t	j;
-	size_t	start;
-	size_t	end;
 
-	i = 0;
-	end = 0;
-	while (i < arr_len)
+	j = 0;
+	while (*str)
 	{
-		j = 0;
-		start = check_start(str, c, end);
-		end = check_end(str, c, start);
-		arr[i] = (char *)malloc(sizeof(char) * (end - start + 2));
-		if (!arr[i])
+		while (*str == c)
+			str++;
+		i = 0;
+		while (str[i] && str[i] != c)
+			i++;
+		array[j] = (char *)malloc(sizeof(char) * (i + 1));
+		if (!array[j])
 		{
-			while (j < i)
-				free(arr[j++]);
+			free_all(array, j);
 			return (NULL);
 		}
-		while (start < end)
-			arr[i][j++] = str[start++];
-		arr[i++][j] = '\0';
+		ft_strlcpy(array[j++], str, (i + 1));
+		str += i;
 	}
-	arr[i] = NULL;
-	return (arr);
+	array[j] = NULL;
+	return (array);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**arr;
+	char	**array;
 	char	*str;
 	size_t	arr_len;
 
 	str = (char *)s;
 	arr_len = word_count(str, c);
-	arr = (char **)malloc(sizeof(char *) * (arr_len + 1));
-	if (!arr)
+	array = (char **)malloc(sizeof(char *) * (arr_len + 1));
+	if (!array)
 		return (NULL);
-	if (!do_split(arr, str, c, arr_len))
-	{
-		free(arr);
+	if (!do_split(array, str, c, arr_len))
 		return (NULL);
-	}
-	return (arr);
+	return (array);
 }
 
-#include <stdio.h>
-int	main(void)
-{
-	char *str;
-	char c;
-	char **ans;
-	int i;
-	int count;
+// #include <stdio.h>
+// int	main(void)
+// {
+// 	char *str;
+// 	char c;
+// 	char **ans;
+// 	int i;
+// 	int count;
 
-	str = "-hello-world--japan---42-42Tokyo";
-	// str = "babaaaaaaaaaaaaaaaa";
-	c = '-';
-	i = 0;
-	count = (word_count(str, c) + 1);
-	printf("count : %d\n", count);
-	ans = ft_split(str, c);
-	printf("%s\n",str);
-	while (i < count)
-	{
-		printf("%d: %s\n", i, ans[i]);
-		i++;
-	}
-}
+// 	str = "-hello-world--japan---42-42Tokyo";
+// 	// str = "babaaaaaaaaaaaaaaaa";
+// 	c = '-';
+// 	i = 0;
+// 	count = (word_count(str, c) + 1);
+// 	printf("count : %d\n", count);
+// 	ans = ft_split(str, c);
+// 	printf("%s\n",str);
+// 	while (i < count)
+// 	{
+// 		printf("%d: %s\n", i, ans[i]);
+// 		i++;
+// 	}
+// }
