d779eb4334ec39a70a7fb5eeb75a2cb1e309aae9
ryhara <aaxouei52@gmail.com>
2023-05-31 10:21:52 +0900

    [Add] lstadd lstnew

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -1,25 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ryhara <ryhara@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/30 22:44:29 by ryhara            #+#    #+#             */
-/*   Updated: 2023/05/30 22:44:29 by ryhara           ###   ########.fr       */
+/*   Created: 2023/05/31 10:12:14 by ryhara            #+#    #+#             */
+/*   Updated: 2023/05/31 10:12:14 by ryhara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstnew(void *content)
+void ft_lstadd_front(t_list **lst, t_list *new)
 {
-	t_list	*new;
-
-	new = (t_list *)malloc(sizeof(t_list));
-	if (new == NULL)
-		return (NULL);
-	new->content = content;
-	new->next = NULL;
-	return (new);
+	if (lst == NULL || new == NULL)
+		return ;
+	new->next = *lst;
+	*lst = new;
 }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+// 	t_list *lst = ft_lstnew("0");
+// 	ft_lstadd_front(&lst, ft_lstnew("1"));
+// 	while (lst->next)
+// 	{
+// 		printf("%s\n",(char *)lst->content);
+// 		lst = lst->next;
+// 	}
+// }
