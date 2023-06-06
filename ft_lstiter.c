8c8389486189927155352568b4df5d20959a1211
ryhara <aaxouei52@gmail.com>
2023-06-06 15:39:56 +0900

    [Add] lstiter lstmap

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -14,5 +14,11 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-
+	if (lst == NULL || f == NULL)
+		return ;
+	while (lst)
+	{
+		f(lst -> content);
+		lst = lst -> next;
+	}
 }
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -14,5 +14,28 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*head;
+	t_list	*new_list;
+	t_list	*new_head;
+
+	if (lst == NULL || f == NULL || del == NULL)
+		return (NULL);
+	head = lst;
+	new_head = ft_lstnew(f(lst->content));
+	if (!new_head)
+		return (NULL);
+	lst = lst->next;
+	while (lst)
+	{
+		new_list = ft_lstnew(f(lst->content));
+		if (!new_list)
+		{
+			ft_lstclear(&head, del);
+			ft_lstclear(&new_head, del);
+			return (NULL);
+		}
+		ft_lstadd_back(new_head, new_list);
+	}
+	ft_lstclear(&head, del);
+	return (new_head);
 }
