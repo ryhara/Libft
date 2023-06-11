8f0c84f2f117547986b33c6b01e1fe8d46a31fa6
Ryosei Hara <ryhara@c1r15s8.42tokyo.jp>
2023-06-11 13:07:59 +0900

    [Fix] putnbr -10

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -17,7 +17,7 @@ void	ft_putnbr_fd(int n, int fd)
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
-		if (n < -10)
+		if (n <= -10)
 			ft_putnbr_fd((n / -10), fd);
 		n = n % 10 * -1;
 	}
@@ -36,6 +36,6 @@ void	ft_putnbr_fd(int n, int fd)
 // 	ft_putchar_fd('\n',1);
 // 	ft_putnbr_fd(INT_MIN,1);
 // 	ft_putchar_fd('\n',1);
-// 	ft_putnbr_fd(-5,1);
+// 	ft_putnbr_fd(-10,1);
 // 	ft_putchar_fd('\n',1);
 // }
